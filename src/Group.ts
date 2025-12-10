849f2c148a5d5b9bcb887d05256683fe689e31c7
Łukasz <skotniczny@outlook.com>
2025-12-10 21:35:43 +0100

    feat: implement Group class for managing match results and calculating standings

---
+++ src/Group.ts
@@ -0,0 +1,85 @@
+export type TableRow = [string, number, number, number];
+export type Result = number | null;
+
+export default class Group {
+  readonly name: string;
+  #results: (Result | undefined)[][] = [
+    [undefined, null, null, null],
+    [null, undefined, null, null],
+    [null, null, undefined, null],
+    [null, null, null, undefined],
+  ];
+  
+  #table: TableRow[] = [];
+
+  constructor(groupName: string, readonly teams: string[]) {
+    if (teams.length !== 4) throw new Error("Require 4 Teams");
+    this.name = `Group ${groupName}`;
+    this.#table = this.#calculateTable()
+  }
+
+  get table(): TableRow[] {
+    return this.#table
+  }
+
+  setScore(homeName: string, awayName: string, result: [home: Result, away: Result]): void {
+    const homeIndex: number = this.teams.indexOf(homeName)
+    const awayIndex: number = this.teams.indexOf(awayName)
+    if (homeIndex === -1 || awayIndex === -1) {
+      throw new Error("Unknown team name");
+    }
+    if (homeIndex === awayIndex) {
+      throw new Error("Team cannot play against itself");
+    } 
+    const [scoreHome, scoreAway] = result;
+    if (scoreHome == null && scoreAway == null) return
+    if (scoreHome != null) {
+      this.#results[homeIndex][awayIndex] = scoreHome
+    }
+    if (scoreAway != null) {
+      this.#results[awayIndex][homeIndex] = scoreAway
+    }
+    this.#table = this.#calculateTable()
+  }
+
+  #calculateTable(): TableRow[] {
+    return this.teams.map((team, index) => {
+      const points: number = this.#sumPoints(index);
+      const goalsFor: number = this.#goalsFor(index);
+      const goalsAgainst: number = this.#goalsAgainst(index);
+      return [team, points, goalsFor, goalsAgainst];
+    });
+  }
+
+  #goalsFor(rowIndex: number): number {
+    return this.#results[rowIndex].reduce<number>((acc, score, columnIndex) => {
+      if (rowIndex === columnIndex) return acc;
+      return acc + (score ?? 0);
+    }, 0);
+  }
+
+  #goalsAgainst(columnIndex: number): number {
+    return this.#results.reduce<number>((acc, row, rowIndex) => {
+      if (rowIndex === columnIndex) return acc;
+      return acc + (row[columnIndex] ?? 0);
+    }, 0);
+  }
+
+  #sumPoints(teamIndex: number): number {
+    let points: number = 0;
+    for (let index = 0; index < this.#results.length; index += 1) {
+      if (teamIndex == index) continue;
+      const home: Result = this.#results[teamIndex][index] ?? null;
+      const away: Result = this.#results[index][teamIndex] ?? null;
+      points += this.#getPoints(home, away);
+    }
+    return points;
+  }
+
+  #getPoints(scoreHome: Result, scoreAway: Result): number {
+    if (scoreHome === null || scoreAway === null) return 0
+    if (scoreHome > scoreAway) return 3;
+    if (scoreHome === scoreAway) return 1;
+    return 0;
+  }
+}
