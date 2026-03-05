125d16926b214e0cda79882cfc52ad0ebb5598b9
Łukasz <skotniczny@outlook.com>
2026-03-05 22:05:22 +0100

    refactor: dynamically generate group links in navigation

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,21 @@
 <script lang="ts">
   import { Router, Link, Route } from "svelte5-router"
+  import { groupNames } from "./data/groups"
   import Group from "./lib/Group.svelte"
   import Home from "./lib/Home.svelte"
   import Standings from "./lib/Standings.svelte";
   import Knockout from "./lib/Knockout.svelte";
 
-  let url:string = $state("")
-  let basepath:string = $state("/world-cup-2026")
+  let url: string = $state("")
+  let basepath: string = $state("/world-cup-2026")
 </script>
 <Router {url} {basepath}>
 <nav class="nav">
   <ul>
     <li><Link to="{basepath}/">Home</Link></li>
-    <li><Link to="{basepath}/group/A">Grupa A</Link></li>
-    <li><Link to="{basepath}/group/B">Grupa B</Link></li>
-    <li><Link to="{basepath}/group/C">Grupa C</Link></li>
-    <li><Link to="{basepath}/group/D">Grupa D</Link></li>
-    <li><Link to="{basepath}/group/E">Grupa E</Link></li>
-    <li><Link to="{basepath}/group/F">Grupa F</Link></li>
-    <li><Link to="{basepath}/group/G">Grupa G</Link></li>
-    <li><Link to="{basepath}/group/H">Grupa H</Link></li>
-    <li><Link to="{basepath}/group/I">Grupa I</Link></li>
-    <li><Link to="{basepath}/group/J">Grupa J</Link></li>
-    <li><Link to="{basepath}/group/K">Grupa K</Link></li>
-    <li><Link to="{basepath}/group/L">Grupa L</Link></li>
+    {#each groupNames as g}
+      <li><Link to="{basepath}/group/{g}">Group {g}</Link></li>
+    {/each}
     <li><Link to="{basepath}/standings">Standings</Link></li>
     <li><Link to="{basepath}/knockout">Knockout</Link></li>
   </ul>
--- src/data/groups.ts
+++ src/data/groups.ts
@@ -3,6 +3,8 @@ import type { TeamInfo } from "./teams"
 
 export type GroupName = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L";
 
+export const groupNames = Object.keys(GROUPS_DATA) as GroupName[]
+
 const groups = new Map<GroupName, TeamInfo[]>(Object.entries(GROUPS_DATA) as [GroupName, TeamInfo[]][])
 
 export function getGroup(name: GroupName): TeamInfo[] {
