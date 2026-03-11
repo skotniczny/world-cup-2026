3b8b8d14970bf035da38f9d316f66e2daa84c84d
Łukasz <skotniczny@outlook.com>
2026-03-11 23:26:38 +0100

    feat: make standings grid responsive

--- src/lib/Standings.svelte
+++ src/lib/Standings.svelte
@@ -13,7 +13,7 @@
 <style>
   .standings-grid {
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-columns: repeat(auto-fit, minmax(var(--wc-grid-col-min-width), 1fr));
     gap: var(--wc-space-lg);
   }
 </style>
