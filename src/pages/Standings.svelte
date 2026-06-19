8634901518042007d7b2d8b6e0ba267d292ccd5b
Łukasz <skotniczny@outlook.com>
2026-06-19 10:11:51 +0200

    feat: add hasUnresolvedTies prop to Table component in Standings

--- src/pages/Standings.svelte
+++ src/pages/Standings.svelte
@@ -6,7 +6,7 @@
 <h1>Standings</h1>
 <div class="standings-grid">
   {#each Object.values(groupsData) as group (group.name)}
-    <Table name={group.name} table={group.table} />
+    <Table name={group.name} table={group.table} hasUnresolvedTies={group.hasUnresolvedTies} />
   {/each}
 </div>
 
